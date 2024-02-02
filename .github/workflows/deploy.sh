7777df55d2716dae855a70cfcea5969cd257c2c0
Derek Tradd <derektradd@dereks-mbp.lan>
2024-02-02 16:42:57 -0500

    Github pipeline: Deploying to aws

---
+++ .github/workflows/deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Additional deployment steps go here
+
+# Ensure we are in the correct directory
+cd $DEPLOYMENT_PATH || exit
+
+# Deploy files from the GitHub workspace to the server
+scp -r $GITHUB_WORKSPACE/* .
