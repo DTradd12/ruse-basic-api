bcef35211b8fa7a92c105d26e4b0f73fdddcc540
Derek Tradd <derektradd@dereks-mbp.lan>
2024-02-05 15:19:04 -0500

    oops

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 echo 'run after_install.sh: ' >> /home/ec2-user/rust-basic-api/deploy.log
 
-echo 'cd /home/ec2-user/myrepo' >> /home/ec2-user/rust-basic-api/deploy.log
-cd /home/ec2-user/myrepo >> /home/ec2-user/rust-basic-api/deploy.log
+echo 'cd /home/ec2-user/rust-basic-api/' >> /home/ec2-user/rust-basic-api/deploy.log
+cd /home/ec2-user/rust-basic-api/ >> /home/ec2-user/rust-basic-api/deploy.log
 
 echo 'cargo build' >> /home/ec2-user/rust-basic-api/deploy.log
 cargo build  >> /home/ec2-user/rust-basic-api/deploy.log
