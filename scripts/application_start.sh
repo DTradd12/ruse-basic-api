16c42f26a7f53617c9471b132d6343b4fe87d764
Derek Tradd <derektradd@dereks-mbp.lan>
2024-02-05 15:16:55 -0500

    test

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ec2-user/myrepo/deploy.log
+echo 'run application_start.sh: ' >> /home/ec2-user/rust-basic-api/deploy.log
 # nodejs-app is the same name as stored in pm2 process
-echo 'cargo run' >> /home/ec2-user/myrepo/deploy.log
-cargo run >> /home/ec2-user/myrepo/deploy.log
+echo 'cargo run' >> /home/ec2-user/rust-basic-api/deploy.log
+cargo run >> /home/ec2-user/rust-basic-api/deploy.log
